f29f42570bdd871870e8c531512a66c57b818a8f
oleg <famouse2016@gmail.com>
2026-04-14 19:52:31 +0300

    feat: initial commit with preprocessor and test program

---
+++ test.kt
@@ -0,0 +1,40 @@
+// Объявление функции
+fun add(a: Int, b: Int): Int {
+    return a + b
+}
+ 
+fun main() {
+    /* Объявление переменных */
+    val x: Int = 10
+    val y: Int = 3
+    var result: Int
+ 
+    // Арифметические выражения
+    result = x + y
+    val product = x * y
+ 
+    // Логическое выражение
+    val isPositive = x > 0 && y > 0
+ 
+    // Условный оператор if-else
+    if (isPositive) {
+        println("Оба положительные")
+    } else {
+        println("Не положительные")
+    }
+ 
+    // Цикл for
+    for (i in 1..5) {
+        println("i = $i")
+    }
+ 
+    // Цикл while
+    var count = 0
+    while (count < 3) {
+        count++
+    }
+ 
+    // Вызов функции
+    result = add(x, y)
+    println("Сумма: $result")
+}
